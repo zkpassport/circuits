56fa8491fdcba52464b24f48d9b264aedcd717aa
Theo Madzou <madztheo@hotmail.fr>
2023-12-17 15:40:16 +0100

    add French and German CSCA certificates

--- index.js
+++ index.js
@@ -6,13 +6,74 @@ function hexToBytes(hex) {
   return bytes;
 }
 
-const res = hexToBytes(
-  "aa55588329ece95c591d5e7aaa621e78ac78b484f2f23e442e97689f9e0c5c42917be63cd3be01ed00532dacfe009461353571e713e69e0dfd4e78e35e9ccbc3370480fe4bcab3520a0460e73adad7f33da4586eaefea4649282360594af16d7ef51745f94cbe4d645d27a5e7054707aaeae588efb99a33405f41717d54d035353f4cb8bd80b809dbc27bf73503900779b678c824cdaa9f3b66085eaa0023c61bc0a71a51c0785d34930bf0a789db0598d560b83750f7b8be18b3e1d766e575fc28902975976e40898fcce8749fa198d2197fc2e22e8fac342096c24e61eb0dae5d73a521602499fb80c0c85996924fde12f4c0362fcfcc2b7e2c59459ea6ebd703db41721387a9600cff5a9aecd33e2e8fa384bc2d6db4f5e4801aabea76392cee78a223781bf90c3ed91db9b28f79383d5caed39b107a4b68ea44572c8307f3083ba11e8773ae8f56b9d4ab63ac714267fc806b7b2fe252260a8071fe106a278c710845cf2efce6ddc9f65e32bb97306e132d308037d5d3803af23e52799aace90d58dbb8735eeb2dcf0f4bd5506bc0f4ac77238baa5640afe6172a16e4767aebc84d1e9f98464faa37a8bc0b7005668bc9ca7ab947991690f8537c12ec2fb46621ffb33c2c20b20475fa09f7a8a531bb8e6010fcc8fb4ee967e54d765d948bb71b8343f157c5df1df4665f0f05eda1c577f7216fa0743aca0dee37cc0a0eb"
+function convertRawCertHex(rawCertHex = "") {
+  return rawCertHex
+    .replaceAll(":", "")
+    .replaceAll(" ", "")
+    .replaceAll("\n", "")
+    .trim();
+}
+
+const publicKey = hexToBytes(
+  convertRawCertHex(`04:5d:15:48:48:4c:22:91:ab:4c:45:70:1d:f7:f0:
+  0b:20:4f:fb:72:3f:5e:50:55:87:e7:ed:cd:cd:62:
+  69:54:e5:9f:c5:fb:f6:41:88:00:07:e7:10:68:10:
+  a6:3d:a1:64:64:eb:d0:33:2c:64:ce:90:93:ae:ba:
+  d4:f4:a8:91:db:a6:05:b8:57:cf:0f:6f:e6:6f:03:
+  e6:6d:14:1e:df:8b:95:2a:98:78:2a:70:4e:36:31:
+  a8:f2:d8:94:f5:74:e6`)
 );
 
+const prime = hexToBytes(
+  convertRawCertHex(`8c:b9:1e:82:a3:38:6d:28:0f:5d:6f:7e:50:e6:
+  41:df:15:2f:71:09:ed:54:56:b4:12:b1:da:19:7f:
+  b7:11:23:ac:d3:a7:29:90:1d:1a:71:87:47:00:13:
+  31:07:ec:53`)
+);
+
+const a = hexToBytes(
+  convertRawCertHex(`7b:c3:82:c6:3d:8c:15:0c:3c:72:08:0a:ce:05:af:
+  a0:c2:be:a2:8e:4f:b2:27:87:13:91:65:ef:ba:91:
+  f9:0f:8a:a5:81:4a:50:3a:d4:eb:04:a8:c7:dd:22:
+  ce:28:26`)
+);
+
+const b = hexToBytes(
+  convertRawCertHex(`04:a8:c7:dd:22:ce:28:26:8b:39:b5:54:16:f0:44:
+  7c:2f:b7:7d:e1:07:dc:d2:a6:2e:88:0e:a5:3e:eb:
+  62:d5:7c:b4:39:02:95:db:c9:94:3a:b7:86:96:fa:
+  50:4c:11`)
+);
+
+const generator = hexToBytes(
+  convertRawCertHex(`04:1d:1c:64:f0:68:cf:45:ff:a2:a6:3a:81:b7:c1:
+  3f:6b:88:47:a3:e7:7e:f1:4f:e3:db:7f:ca:fe:0c:
+  bd:10:e8:e8:26:e0:34:36:d6:46:aa:ef:87:b2:e2:
+  47:d4:af:1e:8a:be:1d:75:20:f9:c2:a4:5c:b1:eb:
+  8e:95:cf:d5:52:62:b7:0b:29:fe:ec:58:64:e1:9c:
+  05:4f:f9:91:29:28:0e:46:46:21:77:91:81:11:42:
+  82:03:41:26:3c:53:15`)
+);
+
+const order = hexToBytes(
+  convertRawCertHex(`8c:b9:1e:82:a3:38:6d:28:0f:5d:6f:7e:50:e6:
+  41:df:15:2f:71:09:ed:54:56:b3:1f:16:6e:6c:ac:
+  04:25:a7:cf:3a:b6:af:6b:7f:c3:10:3b:88:32:02:
+  e9:04:65:65`)
+);
+
+const cofactor = hexToBytes("01");
+
 fs.writeFileSync(
-  "./csca/fr/2015/pkey.json",
+  "./csca/de/2011/cert.json",
   JSON.stringify({
-    publicKey: res,
+    signatureAlgorithm: "ecdsa-with-SHA384",
+    publicKey,
+    prime,
+    a,
+    b,
+    generator,
+    order,
+    cofactor,
   })
 );
