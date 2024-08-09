28982b5b496c95ab0b87b2b7f854b5966f3cb348
Theo Madzou <madztheo@hotmail.fr>
2024-08-09 10:32:43 -0400

    use new rsa and bigint libs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-extern crate noir_rsa_optimised;
+use std::env;
 
-use noir_rsa_optimised::{hashmap_to_toml, get_inputs};
+extern crate noir_bignum_paramgen;
+use noir_bignum_paramgen::bn_runtime_instance_from_string;
+extern crate num_bigint;
+use num_bigint::BigUint;
 
 fn main() {
-    // This is the data that will be signed
-    // It is either the DER encoded TBS certificate for checking the signature
-    // made by CSCA certificate of the issuing state
-    // or the 104 bytes eContent of the passport data
-    let data_to_sign = [];
-    // This is the actual signature, either the encrypted digest of the passport
-    // data or the signature by the CSCA certificate of the DSC DER encoded TBS
-    // certificate
-    let signature = [];
+    //let args: Vec<String> = env::args().collect();
+
+    //let valid_args = args.len() == 4;
     // This is the public key, either the public key of the issuing state CSCA
     // certificate that was used to sign the DSC or the public key of the DSC that 
     // was used to sign the passport data
     let pubkey = [];
-    let hashmap = get_inputs(data_to_sign.to_vec(), signature.to_vec(), pubkey.to_vec());
-    println!("{:?}", hashmap_to_toml(hashmap));   
+
+    let big_int_pub_key = BigUint::from_bytes_be(&pubkey);
+    let string_pub_key = big_int_pub_key.to_str_radix(16);
+    let result = bn_runtime_instance_from_string(string_pub_key, String::from("BigNumInstance"));
+    println!("result: {}", result);
 }
