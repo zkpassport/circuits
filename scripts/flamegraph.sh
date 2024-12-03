825a5a4b9a04c8b04a35528e2c07486f5dae542d
Theo Madzou <madztheo@hotmail.fr>
2024-12-03 17:19:54 +0800

    improve flamegraph generation script

--- scripts/flamegraph.sh
+++ scripts/flamegraph.sh
@@ -5,5 +5,8 @@ package_name=$1
 nargo compile --force --package $package_name
 
 # Generate the flamegraph
-noir-profiler gates-flamegraph --artifact-path ./target/$package_name.json --backend-path $HOME/.bb/bb --output ./flamegraphs/$package_name.svg
-noir-profiler opcodes-flamegraph --artifact-path ./target/$package_name.json --output ./flamegraphs/${package_name}_opcodes.svg
+mkdir -p ./flamegraphs/$package_name
+noir-profiler gates-flamegraph --artifact-path ./target/$package_name.json --backend-path $HOME/.bb/bb --output ./flamegraphs/$package_name
+noir-profiler opcodes-flamegraph --artifact-path ./target/$package_name.json --output ./flamegraphs/$package_name
+open -a "Google Chrome" ./flamegraphs/$package_name/main_acir_opcodes.svg
+open -a "Google Chrome" ./flamegraphs/$package_name/main_gates.svg
