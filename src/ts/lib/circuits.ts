9da9ad7f3c64e9d2a0f96f24c8e2f4e1957bade1
Theo Madzou <madztheo@hotmail.fr>
2024-12-13 18:43:15 +0000

    update integration tests to noir 1.0

--- src/ts/lib/circuits.ts
+++ src/ts/lib/circuits.ts
@@ -1,18 +1,25 @@
 import { Binary } from "@/lib/binary"
 import { BB_THREADS, CERT_TYPE_CSC, CERTIFICATE_REGISTRY_ID, TBS_MAX_SIZE } from "@/lib/constants"
 import { CSC, ECDSACSCPublicKey, RSACSCPublicKey } from "@/types"
-import { BarretenbergSync, Fr } from "@aztec/bb.js"
-import { BarretenbergBackend, ProofData } from "@noir-lang/backend_barretenberg"
+import { BarretenbergSync, Fr, UltraHonkBackend } from "@aztec/bb.js"
 import { CompiledCircuit, InputMap, Noir } from "@noir-lang/noir_js"
+import { ProofData } from "@noir-lang/types"
 import { readFileSync } from "fs"
 import path from "path"
+import { acirToUint8Array, deflattenFields, flattenFieldsAsArray } from "./utils"
+import { decompressSync as gunzip } from "fflate"
 
 const bb = await BarretenbergSync.initSingleton()
 
+const serializedBufferSize = 4
+const fieldByteSize = 32
+const publicInputOffset = 3
+const publicInputsOffsetBytes = publicInputOffset * fieldByteSize
+
 export class Circuit {
   private manifest: CompiledCircuit
-  public witness: any
-  public backend: BarretenbergBackend
+  public witness: Uint8Array
+  public backend: UltraHonkBackend
   public noir: Noir
 
   constructor(manifest: CompiledCircuit) {
@@ -24,7 +31,11 @@ export class Circuit {
 
   async init() {
     if (this.backend) return
-    this.backend = new BarretenbergBackend(this.manifest, { threads: BB_THREADS })
+    const acirBytecodeBase64 = this.manifest.bytecode
+    const acirUncompressedBytecode = acirToUint8Array(acirBytecodeBase64)
+    this.backend = new UltraHonkBackend(acirUncompressedBytecode, {
+      threads: BB_THREADS,
+    })
     this.noir = new Noir(this.manifest)
   }
 
@@ -32,27 +43,68 @@ export class Circuit {
     await this.init()
     if (this.witness) return
     const { witness } = await this.noir.execute(inputs)
-    this.witness = witness
+    // Uncompress the witness
+    this.witness = gunzip(witness)
   }
 
   async prove(inputs: InputMap): Promise<ProofData> {
     await this.init()
     if (!this.witness) await this.solve(inputs)
-    const proof = await this.backend.generateProof(this.witness)
-    return proof
+    const proofWithPublicInputs = await this.backend.generateProof(this.witness)
+    const proofAsStrings = deflattenFields(proofWithPublicInputs.slice(4))
+
+    const numPublicInputs = Number(proofAsStrings[1])
+
+    // Account for the serialized buffer size at start
+    const publicInputsOffset = publicInputsOffsetBytes + serializedBufferSize
+    // Get the part before and after the public inputs
+    const proofStart = proofWithPublicInputs.slice(0, publicInputsOffset)
+    const publicInputsSplitIndex = numPublicInputs * fieldByteSize
+    const proofEnd = proofWithPublicInputs.slice(publicInputsOffset + publicInputsSplitIndex)
+    // Construct the proof without the public inputs
+    const proof = new Uint8Array([...proofStart, ...proofEnd])
+
+    // Fetch the number of public inputs out of the proof string
+    const publicInputsConcatenated = proofWithPublicInputs.slice(
+      publicInputsOffset,
+      publicInputsOffset + publicInputsSplitIndex,
+    )
+    const publicInputs = deflattenFields(publicInputsConcatenated)
+    return {
+      proof,
+      publicInputs,
+    }
   }
 
   async proveRecursiveProof(inputs: InputMap): Promise<{ proof: ProofData; artifacts: any }> {
     const proof = await this.prove(inputs)
     const artifacts = await this.backend.generateRecursiveProofArtifacts(
-      proof,
+      proof.proof,
       proof.publicInputs.length,
     )
     return { proof, artifacts }
   }
 
-  async verify(proof: ProofData) {
+  private reconstructProofWithPublicInputsHonk(proofData: ProofData): Uint8Array {
+    // Flatten publicInputs
+    const publicInputsConcatenated = flattenFieldsAsArray(proofData.publicInputs)
+
+    const proofStart = proofData.proof.slice(0, publicInputsOffsetBytes + serializedBufferSize)
+    const proofEnd = proofData.proof.slice(publicInputsOffsetBytes + serializedBufferSize)
+
+    // Concatenate publicInputs and proof
+    const proofWithPublicInputs = Uint8Array.from([
+      ...proofStart,
+      ...publicInputsConcatenated,
+      ...proofEnd,
+    ])
+
+    return proofWithPublicInputs
+  }
+
+  async verify(proofData: ProofData) {
     await this.init()
+    const proof = this.reconstructProofWithPublicInputsHonk(proofData)
     return await this.backend.verifyProof(proof)
   }
 
--- src/ts/lib/utils.ts
+++ src/ts/lib/utils.ts
@@ -1,7 +1,9 @@
 import { readFile } from "fs/promises"
 import { CompiledCircuit, type Field } from "@noir-lang/types"
 import { createFileManager, compile_program } from "@noir-lang/noir_wasm"
+import { WitnessMap } from "@noir-lang/types"
 import path from "path"
+import { decompressSync as gunzip } from "fflate"
 
 /**
  * Convert a little-endian buffer into a BigInt.
@@ -170,4 +172,88 @@ export function assert(truthy: boolean, errorMsg: string): void {
   if (!truthy) {
     throw new Error(errorMsg)
   }
+}
+
+export function base64Decode(input: string): Uint8Array {
+  if (typeof Buffer !== "undefined") {
+    // Node.js environment
+    return Buffer.from(input, "base64")
+  } else if (typeof atob === "function") {
+    // Browser environment
+    return Uint8Array.from(atob(input), (c) => c.charCodeAt(0))
+  } else {
+    throw new Error("No implementation found for base64 decoding.")
+  }
+}
+
+export function flattenFieldsAsArray(fields: string[]): Uint8Array {
+  const flattenedPublicInputs = fields.map(hexToUint8Array)
+  return flattenUint8Arrays(flattenedPublicInputs)
+}
+
+export function deflattenFields(flattenedFields: Uint8Array): string[] {
+  const publicInputSize = 32
+  const chunkedFlattenedPublicInputs: Uint8Array[] = []
+
+  for (let i = 0; i < flattenedFields.length; i += publicInputSize) {
+    const publicInput = flattenedFields.slice(i, i + publicInputSize)
+    chunkedFlattenedPublicInputs.push(publicInput)
+  }
+
+  return chunkedFlattenedPublicInputs.map(uint8ArrayToHex)
+}
+
+export function witnessMapToPublicInputs(publicInputs: WitnessMap): string[] {
+  const publicInputIndices = [...publicInputs.keys()].sort((a, b) => a - b)
+  const flattenedPublicInputs = publicInputIndices.map((index) => publicInputs.get(index) as string)
+  return flattenedPublicInputs
+}
+
+function flattenUint8Arrays(arrays: Uint8Array[]): Uint8Array {
+  const totalLength = arrays.reduce((acc, val) => acc + val.length, 0)
+  const result = new Uint8Array(totalLength)
+
+  let offset = 0
+  for (const arr of arrays) {
+    result.set(arr, offset)
+    offset += arr.length
+  }
+
+  return result
+}
+
+function uint8ArrayToHex(buffer: Uint8Array): string {
+  const hex: string[] = []
+
+  buffer.forEach(function (i) {
+    let h = i.toString(16)
+    if (h.length % 2) {
+      h = "0" + h
+    }
+    hex.push(h)
+  })
+
+  return "0x" + hex.join("")
+}
+
+function hexToUint8Array(hex: string): Uint8Array {
+  const sanitised_hex = BigInt(hex).toString(16).padStart(64, "0")
+
+  const len = sanitised_hex.length / 2
+  const u8 = new Uint8Array(len)
+
+  let i = 0
+  let j = 0
+  while (i < len) {
+    u8[i] = parseInt(sanitised_hex.slice(j, j + 2), 16)
+    i += 1
+    j += 2
+  }
+
+  return u8
+}
+
+export function acirToUint8Array(base64EncodedBytecode: string): Uint8Array {
+  const compressedByteCode = base64Decode(base64EncodedBytecode)
+  return gunzip(compressedByteCode)
 }
