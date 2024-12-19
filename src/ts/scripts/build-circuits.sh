8fc21251684e76d4b80ad85afd338b1fe89c7b18
Michael Elliot <michael@digital.com.au>
2024-12-20 01:53:58 +0800

    fix ci

--- src/ts/scripts/build-circuits.sh
+++ src/ts/scripts/build-circuits.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-nargo compile --force --package sig_check_dsc_tbs_1500_rsa_pkcs_4096
-nargo compile --force --package sig_check_id_data_tbs_1500_rsa_pkcs_2048
-nargo compile --force --package data_check_integrity
-nargo compile --force --package disclose_bytes
+# Use NARGO_PATH if set, otherwise fall back to 'nargo'
+NARGO=${NARGO_PATH:-nargo}
+
+$NARGO compile --force --package sig_check_dsc_tbs_1500_rsa_pkcs_4096
+$NARGO compile --force --package sig_check_id_data_tbs_1500_rsa_pkcs_2048
+$NARGO compile --force --package data_check_integrity
+$NARGO compile --force --package disclose_bytes
