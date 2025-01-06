9d45fb62499482239ce3ac1d62879ed379c4e1d0
Michael Elliot <michael@digital.com.au>
2025-01-06 18:48:23 +0800

    fix ci

--- scripts/ci-compile-circuits.sh
+++ scripts/ci-compile-circuits.sh
@@ -2,14 +2,23 @@
 
 # This script compiles a subset of Noir circuits required for CI pipeline.
 
-set -e
-BIN=${NARGO_PATH:-nargo}
+set -euo pipefail
+
+: ${NARGO_BIN:=nargo}
+: ${BUN_BIN:=bun}
 
 # Update the circuits to be unconstrained, so that we can test the logic
 # of the circuit for given inputs without doing fully constrained proving.
-bun run src/ts/scripts/circuit-builder.ts unconstrained
+$BUN_BIN run src/ts/scripts/circuit-builder.ts unconstrained
 
-$BIN compile --force --package sig_check_dsc_tbs_1500_rsa_pkcs_4096
-$BIN compile --force --package sig_check_id_data_tbs_1500_rsa_pkcs_2048
-$BIN compile --force --package data_check_integrity
-$BIN compile --force --package disclose_flags
+# Circuits to compile
+CIRCUITS=(
+    "sig_check_dsc_tbs_1500_rsa_pkcs_4096"
+    "sig_check_id_data_tbs_1500_rsa_pkcs_2048"
+    "data_check_integrity"
+    "disclose_flags"
+)
+
+for circuit in "${CIRCUITS[@]}"; do
+    $NARGO_BIN compile --force --package "$circuit"
+done
