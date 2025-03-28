e41eb9a088c2b6c04e85a18ccbd35d279b9ea69d
Théo Madzou <17496111+madztheo@users.noreply.github.com>
2025-03-28 13:29:04 +0000

    Update to Barretenberg 0.82 (#39)
    * update to barretenberg 0.82, update rsa, ecdsa and bignum libs
    
    * update bb commands to new api

--- scripts/prove-honk.sh
+++ scripts/prove-honk.sh
@@ -4,5 +4,7 @@ package_name=$1
 # Execute the circuit to generate a witness
 nargo execute --package $package_name ${package_name}_witness
 
+mkdir -p ./proofs/${package_name}_honk
+
 # Generate a Honk proof for the circuit with the witness generated just before
-time bb prove_ultra_honk -b ./target/$package_name.json -w ./target/${package_name}_witness.gz -o ./proofs/${package_name}_honk.proof
+time bb prove --scheme ultra_honk -b ./target/$package_name.json -w ./target/${package_name}_witness.gz -o ./proofs/${package_name}_honk
