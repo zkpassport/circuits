7d46c6cbc9037043818d8b4dafb79464e483c6cd
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-08-25 14:38:08 +0000

    chore: update constants.sol

--- src/solidity/src/Constants.sol
+++ src/solidity/src/Constants.sol
@@ -5,15 +5,15 @@ pragma solidity >=0.8.21;
 bytes32 constant SANCTIONS_TREES_ROOT = 0x27cea23b989f5246d6577568d11cff22537f10fb47729dc004d1bf464ce37bd3;
 
 library CommittedInputLen {
-    uint256 constant SANCTIONS = 33;
-    uint256 constant EXCL_NATIONALITY = 601;
-    uint256 constant INCL_NATIONALITY = 601;
-    uint256 constant BIND = 501;
-    uint256 constant COMPARE_AGE = 11;
-    uint256 constant COMPARE_BIRTHDATE = 25;
-    uint256 constant COMPARE_EXPIRY = 25;
+    uint256 constant COMPARE_AGE = 7;
+    uint256 constant COMPARE_BIRTHDATE = 13;
+    uint256 constant COMPARE_EXPIRY = 13;
     uint256 constant DISCLOSE_BYTES = 181;
     uint256 constant INCL_ISSUING_COUNTRY = 601;
     uint256 constant EXCL_ISSUING_COUNTRY = 601;
+    uint256 constant INCL_NATIONALITY = 601;
+    uint256 constant EXCL_NATIONALITY = 601;
+    uint256 constant BIND = 501;
+    uint256 constant SANCTIONS = 33;
 }
 
