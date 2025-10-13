60ef648f2fb27b94eef20960146d4d9b1cac8447
Theo Madzou <madztheo@hotmail.fr>
2025-10-13 19:03:18 +0100

    commit to play integrity public key in facematch android circuits

--- scripts/info.sh
+++ scripts/info.sh
@@ -1,5 +1,7 @@
 # Get the name of the package from the argument
 package_name=$1
 
+time nargo compile --force --package $package_name
+
 # Compile the circuit and get the number of the gates
 time bb gates --scheme ultra_honk -b ./target/$package_name.json > ./info/$package_name.json
